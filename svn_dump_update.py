fe7fcf75728edd535c18ad7ed86b1a4a794841c9
Nitesh Turaga <nitesh.turaga@gmail.com>
2017-04-01 09:03:23 +0530

    annotation data additions

--- svn_dump_update.py
+++ svn_dump_update.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Bioconductor update SVN dump code.
 
 Author: Nitesh Turaga
@@ -28,13 +27,32 @@ def svn_root_update(configfile):
     update_file = Config.get('SVN', 'update_file')
 
     for s in Config.sections():
-       for k,v in Config.items(s):
-           log.info("%s: %s" % (k,v))
+        for k, v in Config.items(s):
+            log.info("%s: %s" % (k, v))
 
     dump = LocalSvnDump(svn_root, bioc_git_repo, users_db, remote_svn_server)
     dump.svn_get_revision()
     dump.svn_dump_update(update_file)
     dump.update_local_svn_dump(update_file)
+    return
+
+
+def svn_bioc_data_update(configfile):
+    """Annotation data dump update."""
+    Config = ConfigParser.ConfigParser()
+    Config.read(configfile)
+
+    bioc_data_git_repo = Config.get('Annotation', 'bioc_data_git_repo')
+    svn_annotation_root = Config.get('Annotation', 'svn_annotation_root')
+    remote_svn_data_server = Config.get('Annotation', 'remote_svn_data_server')
+    data_users_db = Config.get('Annotation', 'data_users_db')
+    update_file_data = Config.get('Annotation', 'updata_file_data')
+
+    dump = LocalSvnDump(svn_annotation_root, bioc_data_git_repo,
+                        data_users_db, remote_svn_data_server)
+    dump.svn_get_revision()
+    dump.svn_dump_update(update_file_data)
+    dump.update_local_svn_dump(update_file_data)
     return
 
 
