acc7d39a86fb7eae4eb757bb1472fd6f8c0ad3a7
Martin Morgan <martin.morgan@roswellpark.org>
2017-04-06 17:37:25 -0400

    move data (file paths) to top

--- R/munge_user_db.R
+++ R/munge_user_db.R
@@ -1,9 +1,13 @@
 ## svn log -q $svn | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > users.txt
 
+fin1 <- "bioc_git_transition/extdata/users.txt"
+fin2 <- "bioc_git_transition/extdata/user_db.csv"
+fout <- "bioc_git_transition/extdata/user_db.txt"
+
 txt = read.csv(
-    "users.txt",sep=" ",stringsAsFactors = FALSE,strip.white = T,header = FALSE
+    fin1,sep=" ",stringsAsFactors = FALSE,strip.white = T,header = FALSE
 )
-csv = read.csv("user_db.csv",stringsAsFactors = FALSE)
+csv = read.csv(fin2,stringsAsFactors = FALSE)
 
 present = txt$V1 %in% csv$SVN.User.ID
 
@@ -47,8 +51,6 @@ result = result[-1543]
 
 result = data.frame(result)
 
-result = rbind(result, 
-
 write.table(
-    result, file="user_db.txt",col.names=FALSE, row.names=FALSE, quote=FALSE
+    result, file=fout,col.names=FALSE, row.names=FALSE, quote=FALSE
 )
